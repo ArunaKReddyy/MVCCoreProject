02042f905acd7a74a02b71b53e7809932eef27fa
Admin <Admin@DESKTOP-01G6KAT>
2022-12-08 23:26:53 +0530

    Day2

---
+++ MVCCoreProject/MVCCoreProject/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreProject.Models;
+using MVCCoreProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreProject.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult GetAllBooks()
+        {
+            var data=_bookRepository.getAllBooks();
+            return View(data);
+        }
+        public ViewResult GetBook(int id)
+        {
+            var data =_bookRepository.getBook(id);
+            return View(data);
+            
+        }
+        public List<BookModel> GetBookwithName(string autorname,string title)
+        {
+            return _bookRepository.getBook(autorname,title);
+
+        }
+    }
+}
---
+++ MVCCoreProject/MVCCoreProject/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using MVCCoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreProject.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> getAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel getBook(int id)
+        {
+            var book = DataSource().Where(x => x.Id == id).FirstOrDefault();
+            return book;
+        }
+        public List<BookModel> getBook(string author,string title)
+        {
+            var book = DataSource().Where(x => x.AuthorName.Contains(author) || x.Title.Contains(title)).ToList();
+            return book;
+        }
+        public List<BookModel> DataSource()
+        {
+            var booklist = new List<BookModel>()
+            {
+                new BookModel(){ Id=1,AuthorName="ArunaKReddy",Title="MVC",Description="This is the book to learn MVC",Language="English",Totalpages="1298",Category="Framework"},
+                 new BookModel(){ Id=2,AuthorName="ArunaKReddy",Title="c#",Description="This is the book to learn C#" ,Language="English",Totalpages="198",Category="Language"},
+                  new BookModel(){ Id=3,AuthorName="ArunaKReddy",Title="Python",Description="This is the book to learn Python",Language="English",Totalpages="1277",Category="Programming" },
+                   new BookModel(){ Id=4,AuthorName="ArunaKReddy",Title="WebAPI" ,Description="This is the book to learn WebAPI",Language="English",Totalpages="444",Category="Framework"},
+                    new BookModel(){ Id=5,AuthorName="ArunaKReddy",Title="SQL" ,Description="This is the book to learn SQL",Language="English",Totalpages="899",Category="Language"},
+            };
+            return booklist;
+        }
+    }
+}
