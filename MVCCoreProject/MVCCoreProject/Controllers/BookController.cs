1ff524b01b8293019e54ff786d20ec69056f73f1
Admin <Admin@DESKTOP-01G6KAT>
2022-12-10 00:06:55 +0530

    Dropdown with SelectListItem

--- MVCCoreProject/MVCCoreProject/Controllers/BookController.cs
+++ MVCCoreProject/MVCCoreProject/Controllers/BookController.cs
@@ -40,7 +40,21 @@ namespace MVCCoreProject.Controllers
         [HttpGet]
         public ViewResult AddBook( bool isSuccess=false,int bookid=0)
         {
-            ViewBag.LanguageList = new SelectList(GetLanguages(),"Id", "Text");
+            //ViewBag.LanguageList = new SelectList(GetLanguages(),"Id", "Text");
+            //ViewBag.LanguageList = GetLanguages().Select(x => new SelectListItem()
+            //{
+            //    Value = x.Id.ToString(),
+            //    Text = x.Text
+            //});
+            ViewBag.LanguageList = new List<SelectListItem>()
+            {
+                new SelectListItem(){Value="1", Text="English"},
+                new SelectListItem(){Value="2", Text="Telugu"},
+                new SelectListItem(){Value="3", Text="Hindhi"},
+                new SelectListItem(){Value="4", Text="Tamil" ,Disabled=true},
+                new SelectListItem(){Value="5", Text="Malayalam",Disabled=true},
+                new SelectListItem(){Value="6", Text="Kannada", Selected=true},
+            };
             ViewBag.isSuccess = isSuccess;
             ViewBag.BookId = bookid;
             return View();
@@ -56,7 +70,12 @@ namespace MVCCoreProject.Controllers
                     return RedirectToAction(nameof(AddBook), new { isSuccess = true, bookid = id });
                 }
             }
-            ViewBag.LanguageList = new SelectList(GetLanguages(), "Id", "Text");
+            //ViewBag.LanguageList = new SelectList(GetLanguages(), "Id", "Text");
+            //ViewBag.LanguageList = GetLanguages().Select(x => new SelectListItem()
+            //{
+            //    Value = x.Id.ToString(),
+            //    Text = x.Text
+            //});
             ModelState.AddModelError("", "this is my custom error message");
             return View();
         }
